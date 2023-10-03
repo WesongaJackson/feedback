c7243b587df92f4cd9fc029651cdaa6bf0b42fa9
JACKSON WESONGA <120163285+WesongaJackson@users.noreply.github.com>
2023-10-03 22:44:37 +0300

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,32 @@
+<?php  include 'config/database.php'; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Leave Feedback</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+  <main>
+  <nav class="navbar navbar-expand-lg bg-light">
+     <div class="container-fluid">
+          <a class="navbar-brand " href="#">Beabrand</a>
+       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+       </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto  ">
+             <li class="nav-item">
+               <a class="nav-link"  href="index.php">Home</a>
+             </li>
+              <a class="nav-link" href="about.php">About</a>
+               </li>
+                <li class="nav-item">
+                 <a class="nav-link" href="feedback.php">Feedback</a>
+                </li>
+            </ul>
+             </div>
+      </div>
+    </nav> 
+       <div class="container d-flex flex-column align-items-center">
